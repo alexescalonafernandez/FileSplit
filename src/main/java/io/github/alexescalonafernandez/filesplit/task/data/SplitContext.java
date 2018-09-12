1cc4b99b8897798145fcb041c5e566fd8a6b9256
Alexander Escalona Fernandez <alexescalonafernandez@gmail.com>
2018-09-11 23:35:57 -0400

    feat: implements Builder pattern in SplitContext class

--- src/main/java/io/github/alexescalonafernandez/filesplit/task/data/SplitContext.java
+++ src/main/java/io/github/alexescalonafernandez/filesplit/task/data/SplitContext.java
@@ -9,11 +9,11 @@ public class SplitContext {
     private long beginFilePointer, endFilePointer;
     private SplitTaskConfiguration configuration;
     private long timestamp;
+    private String fileHeader;
 
-    private SplitContext(long beginFilePointer, long endFilePointer, long timestamp, SplitTaskConfiguration configuration) {
+    private SplitContext(long beginFilePointer, long endFilePointer, SplitTaskConfiguration configuration) {
         this.beginFilePointer = beginFilePointer;
         this.endFilePointer = endFilePointer;
-        this.timestamp = timestamp;
         this.configuration = configuration;
     }
 
@@ -55,5 +55,74 @@ public class SplitContext {
 
     public String getRegex() {
         return configuration.getRegex();
+    }
+
+    public String getFileHeader() {
+        return fileHeader;
+    }
+
+    public static class Builder {
+        private long beginFilePointer, endFilePointer;
+        private SplitTaskConfiguration configuration;
+        private long timestamp;
+        private String fileHeader;
+
+        public Builder(long beginFilePointer, long endFilePointer, SplitTaskConfiguration configuration) {
+            this.beginFilePointer = beginFilePointer;
+            this.endFilePointer = endFilePointer;
+            this.configuration = configuration;
+        }
+
+        public long getBeginFilePointer() {
+            return beginFilePointer;
+        }
+
+        public Builder beginFilePointer(long beginFilePointer) {
+            this.beginFilePointer = beginFilePointer;
+            return this;
+        }
+
+        public long getEndFilePointer() {
+            return endFilePointer;
+        }
+
+        public Builder endFilePointer(long endFilePointer) {
+            this.endFilePointer = endFilePointer;
+            return this;
+        }
+
+        public SplitTaskConfiguration getConfiguration() {
+            return configuration;
+        }
+
+        public Builder setConfiguration(SplitTaskConfiguration configuration) {
+            this.configuration = configuration;
+            return this;
+        }
+
+        public long getTimestamp() {
+            return timestamp;
+        }
+
+        public Builder setTimestamp(long timestamp) {
+            this.timestamp = timestamp;
+            return this;
+        }
+
+        public String getFileHeader() {
+            return fileHeader;
+        }
+
+        public Builder fileHeader(String fileHeader) {
+            this.fileHeader = fileHeader;
+            return this;
+        }
+
+        public SplitContext build() {
+            SplitContext context = new SplitContext(beginFilePointer, endFilePointer, configuration);
+            context.timestamp = this.timestamp;
+            context.fileHeader = this.fileHeader;
+            return context;
+        }
     }
 }
