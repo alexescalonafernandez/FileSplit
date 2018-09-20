366ac6428e136e727087bdc588539d89a47c3080
Alexander Escalona Fernández <alexescalonafernandez@gmail.com>
2018-09-20 16:49:51 -0400

    feat: add the exit program option

--- src/main/java/io/github/alexescalonafernandez/filesplit/Main.java
+++ src/main/java/io/github/alexescalonafernandez/filesplit/Main.java
@@ -1,16 +1,18 @@
 package io.github.alexescalonafernandez.filesplit;
 
-import io.github.alexescalonafernandez.filesplit.api.OperationMode;
-import io.github.alexescalonafernandez.filesplit.api.SplitTaskConfiguration;
-import io.github.alexescalonafernandez.filesplit.behaviour.*;
-import org.apache.commons.io.FileUtils;
+import io.github.alexescalonafernandez.filesplit.api.SplitTaskConfigurationFromArgs;
+import io.github.alexescalonafernandez.filesplit.api.SplitTaskConfigurationUtils;
+import io.github.alexescalonafernandez.filesplit.behaviour.BaseMode;
+import io.github.alexescalonafernandez.filesplit.behaviour.NativeTerminalMode;
+import io.github.alexescalonafernandez.filesplit.behaviour.TextIoTerminalMode;
+import io.github.alexescalonafernandez.filesplit.behaviour.TextIoTerminalWithFileChooserMode;
 import org.beryx.textio.swing.SwingTextTerminal;
 
-import java.io.*;
-import java.util.HashMap;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.util.Optional;
-import java.util.regex.Pattern;
-import java.util.regex.PatternSyntaxException;
 
 /**
  * Created by alexander.escalona on 11/09/2018.
@@ -39,22 +41,26 @@ public class Main {
                 e.printStackTrace();
             }
         } else {
-            SplitTaskConfiguration baseTaskConfiguration = buildTaskConfigurationFromArgs(args);
-            BaseMode terminal;
-            if(BaseInteractiveMode.canRunWithoutUserInteraction(baseTaskConfiguration)) {
-                terminal = new NativeTerminalMode(baseTaskConfiguration);
+            SplitTaskConfigurationFromArgs splitTaskConfigurationFromArgs = new SplitTaskConfigurationFromArgs(args);
+            BaseMode terminal = null;
+            if(SplitTaskConfigurationUtils.canRunWithoutUserInteraction(splitTaskConfigurationFromArgs)) {
+                terminal = new NativeTerminalMode(splitTaskConfigurationFromArgs);
             } else {
                 System.setProperty("org.beryx.textio.TextTerminal", SwingTextTerminal.class.getName());
                 int mode = -1;
+                System.out.println("For running without user interaction run with --help argument for more details");
+                System.out.println();
+                System.out.println();
                 BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
                 do {
                     System.out.println();
                     System.out.println("Select the interaction mode");
                     System.out.println("1. Single Terminal Mode");
                     System.out.println("2. Terminal with File Chooser Mode");
+                    System.out.println("3. Exit program");
                     try {
                         mode = Optional.ofNullable(br.readLine())
-                                .filter(s -> s.matches("^1|2$"))
+                                .filter(s -> s.matches("^1|2|3$"))
                                 .map(s -> Integer.valueOf(s))
                                 .orElse(-1);
                     } catch (IOException e) {
@@ -62,165 +68,13 @@ public class Main {
                     }
                 } while (mode == -1);
                 if(mode == 1) {
-                    terminal = new TextIoTerminalMode(baseTaskConfiguration);
-                } else {
-                    terminal = new TextIoTerminalWithFileChooserMode(baseTaskConfiguration);
+                    terminal = new TextIoTerminalMode(splitTaskConfigurationFromArgs);
+                } else if (mode == 2){
+                    terminal = new TextIoTerminalWithFileChooserMode(splitTaskConfigurationFromArgs);
                 }
             }
-            terminal.run();
+            Optional.ofNullable(terminal).ifPresent(baseMode -> baseMode.run());
         }
 
-    }
-
-    private static HashMap<String, String> getArguments(String[] args) {
-        HashMap<String, String> arguments = new HashMap<>();
-        for(int i = 0; i < args.length - 1; i+=2) {
-            arguments.put(args[i], args[i + 1]);
-        }
-
-        return arguments;
-    }
-
-    private static SplitTaskConfiguration buildTaskConfigurationFromArgs(String[] args) {
-        final HashMap<String, String> arguments = getArguments(args);
-        return new SplitTaskConfiguration() {
-            @Override
-            public String getFilePath() {
-                return Optional.ofNullable(arguments.get("--filePath"))
-                        .filter(filePath -> {
-                            File file = new File(filePath);
-                            return file.exists() && file.isFile();
-                        })
-                        .orElse(null);
-            }
-
-            @Override
-            public Long getChunkSize() {
-                long oneKB = 1024 * 1024;
-                return Optional.ofNullable(
-                        Optional.ofNullable(arguments.get("--chunkSize")).orElse(String.valueOf(oneKB)))
-                        .filter(s -> {
-                            try {
-                                long value = Long.valueOf(s);
-                                long oneGB = (long)Math.pow(1024, 4);
-                                return value > 0 && value < oneGB;
-                            } catch (NumberFormatException e) {
-                                return false;
-                            }
-                        }).map(Long::valueOf)
-                        .orElse(null);
-            }
-
-            @Override
-            public Boolean appendFirstLine() {
-                return Boolean.valueOf(arguments.get("--appendFirstLine"));
-            }
-
-            @Override
-            public String getFolderPath() {
-                return Optional.ofNullable(arguments.get("--folderPath"))
-                        .filter(filePath -> {
-                            File file = new File(filePath);
-                            if(file.exists() && file.isDirectory()){
-                                return true;
-                            } else {
-                                try {
-                                    FileUtils.forceMkdir(file);
-                                    return true;
-                                } catch (IOException e) {
-                                    return false;
-                                }
-                            }
-                        })
-                        .orElse(null);
-            }
-
-            @Override
-            public Integer getThreadNumber() {
-                return Optional.ofNullable(
-                        Optional.ofNullable(arguments.get("--threadNumber")).orElse("8"))
-                        .filter(s -> {
-                            try {
-                                int value = Integer.valueOf(s);
-                                return value > 0;
-                            } catch (NumberFormatException e) {
-                                return false;
-                            }
-                        })
-                        .map(Integer::valueOf)
-                        .orElse(null);
-            }
-
-            @Override
-            public Integer getMaxThreadNumber(int limit) {
-                return getThreadNumber();
-            }
-
-            @Override
-            public OperationMode getOperationMode() {
-                return Optional.ofNullable(arguments.get("--mode"))
-                        .map(value -> {
-                            try {
-                                return OperationMode.valueOf(value.toUpperCase());
-                            } catch (IllegalArgumentException e) {
-                                return null;
-                            }
-                        })
-                        .filter(mode -> !OperationMode.ANY.equals(mode))
-                        .orElse(null);
-            }
-
-            @Override
-            public Integer getMaxLines() {
-                return Optional.ofNullable(
-                        Optional.ofNullable(arguments.get("--maxLines")).orElse("1000000"))
-                        .filter(s -> {
-                            try {
-                                int value = Integer.valueOf(s);
-                                return value > 0;
-                            } catch (NumberFormatException e) {
-                                return false;
-                            }
-                        })
-                        .map(Integer::valueOf)
-                        .orElse(null);
-            }
-
-            @Override
-            public String getPrefix() {
-                return Optional.ofNullable(arguments.get("--prefix"))
-                        .filter(prefix -> prefix.trim().length() > 0)
-                        .orElse(null);
-            }
-
-            @Override
-            public String getRegex() {
-                return Optional.ofNullable(arguments.get("--regex"))
-                        .filter(regex -> {
-                            try {
-                                Pattern.compile(regex);
-                                return true;
-                            } catch (PatternSyntaxException ex) {
-                                return false;
-                            }
-                        })
-                        .orElse(null);
-            }
-
-            @Override
-            public Integer getRegexGroup() {
-                return Optional.ofNullable(arguments.get("--regexGroup"))
-                        .filter(s -> {
-                            try {
-                                int value = Integer.valueOf(s);
-                                return value > 0 && value < 10;
-                            } catch (NumberFormatException e) {
-                                return false;
-                            }
-                        })
-                        .map(Integer::valueOf)
-                        .orElse(null);
-            }
-        };
     }
 }
